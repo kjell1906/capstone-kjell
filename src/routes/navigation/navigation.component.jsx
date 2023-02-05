27eb956501950a587e379ef9f33dead855e80028
Kjell Dirdal <78570262+kjell1906@users.noreply.github.com>
2023-02-05 21:20:09 +0100

    added firebase and yarn

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,21 +1,29 @@
-import React, {Fragment} from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import React, { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import "./navigation.styles.scss";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 const Navigation = () => {
-    return <Fragment>
-      <div className='navigation'>
-      <Link className='logo-container' to='/'>
-        <div>Logo</div>
-      </Link>
-      <div className='nav-links-container'>
-        <Link className='nav-link' to='/shop'>
-          SHOP
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
         </Link>
-      </div>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
+        </div>
       </div>
       <Outlet />
-    </Fragment>;
-  
-  };
+    </Fragment>
+  );
+};
 
-  export default Navigation;
+export default Navigation;
