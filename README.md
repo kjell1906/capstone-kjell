1d18a269bae3e844639cd351e3fffdc9481c6f66
Kjell Dirdal <78570262+kjell1906@users.noreply.github.com>
2023-02-05 19:25:59 +0100

    Update

---
+++ README.md
@@ -0,0 +1,3 @@
+# capstone
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-dnibbm)
---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Home from './routes/home/home.component';
+
+const Navigation = () => {
+  return <div>
+    <div>
+    <h1>I am the Navigation page</h1>
+    </div>
+    <Outlet />
+  </div>;
+
+};
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
