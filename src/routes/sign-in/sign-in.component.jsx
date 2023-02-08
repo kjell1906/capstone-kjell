ca6966e564f2d270e07da3d4339cb1ed57249fa5
IN\ANO10112 <kjell.dirdal@azets.com>
2023-02-08 12:23:14 +0100

    Removed useEffect code

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import {getRedirectResult} from 'firebase/auth';
+
 import {
+    auth,
     signInWithGooglePopup,
     createUserProfileDocument,
     signInWitGoogleRedirect,
@@ -8,21 +11,17 @@ import {
   import {createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils';
 
   const SignIn = () => {
+
     const logGoogleUser = async () => {
       const {user} = await signInWithGooglePopup();
       const userDocRef = createUserDocumentFromAuth(user);
-    };
-  
-    const logGoogleRedirectUser = async () => {
-      const {user} = await signInWitGoogleRedirect();
-      console.log({user})
     };
 
     return (
       <div>
         <h1>Sign In Page</h1>
         <button onClick={logGoogleUser}>Sign in with Google Popup</button><br />
-        <button onClick={logGoogleRedirectUser}>Sign in with Google Redirect</button>
+        <button onClick={signInWitGoogleRedirect}>Sign in with Google Redirect</button>
       </div>
     );
   };
