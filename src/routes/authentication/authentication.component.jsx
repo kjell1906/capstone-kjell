4f2769984247e4ff8e6cb1561e27bf841c85a15a
IN\ANO10112 <kjell.dirdal@azets.com>
2023-02-09 14:41:05 +0100

    Update of repo

---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+  
+  const Authentication = () => {
+
+    return (
+      <div>
+        <h1>Authentication Page</h1>
+        <SignInForm />
+        <SignUpForm />
+      </div>
+    );
+  };
+  
+  export default Authentication;
